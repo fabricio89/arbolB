ff037e3c7c869688448da9f3f3a860b14689d337
fab_buitrago <esefue@gmail.com>
2015-09-05 18:14:59 -0500

    fabian

--- src/arbol/b/ArbolB.java
+++ src/arbol/b/ArbolB.java
@@ -6,6 +6,8 @@
 
 package arbol.b;
 
+import java.util.Stack;
+
 /**
  *
  * @author FabianP
@@ -48,12 +50,40 @@ public class ArbolB {
         return i;
     }
     
-    int donde(Pagina p, int x){  //
+    int donde(Pagina p, int x){  //inserta un dato en la pagina dada
         int i=0;
         while(x>p.info[i] && i<p.cont -1)
             i++;
                     return i;
     }
+    
+    //Inserta x en la pagina p
+   // i=donde(p,x);
+    //i=insertar(p,x,i);
+    
+    //INsertar el numero almacenado
+    
+    int Buscar (Pagina p, int x, Stack pila){
+        int i=-1, posicion=-1;
+        boolean encontro= false;
+        
+        while (p !=null && !encontro){
+            pila.push(p);
+            i=0;
+            
+            while(x> p.info [i] && i< p.cont -1)
+                i++;
+            if(x<p.info[i])
+                p=p.apunt[i];
+            else if (x>p.info[i])
+                p=p.apunt[i+1];
+            else encontro=true;
+        }
+    if (!encontro){
+    posicion =i;
+}
+    return posicion;
+}
     
     public static void main(String[] args) {
         // TODO code application logic here
